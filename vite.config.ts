5b37f9ba155cda59efaa9a29807b7309239c71b7
eymen160 <eymenfaruk479@gmail.com>
2026-03-05 02:34:31 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,7 @@ import { defineConfig } from "vite";
 import path from "path";
 
 export default defineConfig({
-  server: {
-    host: "::",
-    port: 8080,
-  },
+  server: { host: "::", port: 8080 },
   build: {
     rollupOptions: {
       input: path.resolve(__dirname, "index.html"),
